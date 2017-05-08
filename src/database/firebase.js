f5b7b08b500625fb02830984aded2b6765428138
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-05-08 18:02:01 +0200

    add database config files and tokens

---
+++ src/database/firebase.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jonlazarini on 06/05/17.
+ */
+/**
+ * **************************************************************
+ *  Can also set the tokens as local shell vars -  ~/.zprofile  *
+ * **************************************************************
+ *
+ * View your shell vars: cat ~/.zprofile
+ * Edit your file: vim  ~/.zprofile
+ * export YOUR_KEY_VARIABLE=<your key>
+ *
+ * In your project: source ~/.zprofile
+ *
+ * Debug
+ * Make sure the env variables are in your shell, run: env
+ *
+ */
+import firebase from 'firebase';
+import {tokens} from '../config/tokens';
+
+var config = {
+    apiKey: tokens.apiKey,
+    authDomain: tokens.authDomain,
+    databaseURL: tokens.databaseURL,
+    projectId: tokens.projectId,
+    storageBucket: tokens.storageBucket,
+    messagingSenderId: tokens.messagingSenderId
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
